79032a548df9576d087e2012c77e89c36cc87bdb
llavenovemiel <llavenovemiel@gmail.com>
2018-10-28 11:27:48 +0800

    includes capstone 1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,74 +1,60 @@
-// $(document).ready(function(){
-//     $("#awards").click(function(){
-//         $(".right-content").toggleClass("right-content-shown");
-//     });
-// });
-
-// $("#awards").click(function(){
-//     $(".right-content-container").slideToggle("slow");
-// });
-
 // for testimonials
-const testimonials = [["assets/images/testi-1.jpg","'Innovative. Best service'","-Donny Tramp, Boston Real Estate"],
-					["assets/images/testi-2.jpg","'Delivered great quality and ahead of time.'","-Brack Obama, Brack Group of Companies"],
-					["assets/images/testi-3.jpg","'Their technical know-how is unparalled.'","-Gloria Royo, Royo and Associates"],
-					["assets/images/testi-4.jpg","'We trust no one else when it comes to bridges.'","-Mark Cruz, Flash Construction"],
-					["assets/images/testi-5.jpg","'Quality, innovation and customer service.'","-Abby Leigh, Leigh & Lark"]];
+const testimonials = [["assets/images/testi-1.jpg","'Innovative. Best service'","-Donny Tramp, BRE"],
+					["assets/images/testi-2.jpg","'Delivered great quality.'","-John Doe, Doe Group"],
+					["assets/images/testi-3.jpg","'Unparalled service.'","-Gloria Royo, R&A"],
+					["assets/images/testi-4.jpg","'We trust no one else.'","-Mark Cruz, Flash Construction"],
+					["assets/images/testi-5.jpg","'Quality and innovation.'","-Abby Leigh, Leigh & Lark"]];
 let index = 0;					
 const loopTestimonial = function() {
 	if (index >= testimonials.length) {
 		index = 0;
 		loopTestimonial();
 	} else {
-		$("#testi-image").attr("src", testimonials[index][0]);
-		$("#testi-text").text(testimonials[index][1]);
-		$("#testi-name").text(testimonials[index][2]);
+		$("#testi-image1").attr("src", testimonials[index][0]);
+		$("#testi-text1").text(testimonials[index][1]);
+		$("#testi-name1").text(testimonials[index][2]);
+        $("#testi-image2").attr("src", testimonials[(index+1>=testimonials.length)?0:index+1][0]);
+        $("#testi-text2").text(testimonials[(index+1>=testimonials.length)?0:index+1][1]);
+        $("#testi-name2").text(testimonials[(index+1>=testimonials.length)?0:index+1][2]);
 		index++;
-		setTimeout(loopTestimonial, 2000);
+		setTimeout(loopTestimonial, 2500);
 	}
 }
 loopTestimonial();
-//
 
-$(document).ready(function(){
-    $(".expertise-1").hover(function(){
-        $(".expertise-img-1").css("display", "inline");
-        }, function(){
-        $(".expertise-img-1").css("display", "none");
-    });
-});
 
-$(document).ready(function(){
-    $(".expertise-2").hover(function(){
-        $(".expertise-img-2").css("display", "inline");
-        }, function(){
-        $(".expertise-img-2").css("display", "none");
-    });
-});
+// for carousel
+var carousel = document.querySelector('.carousel');
+var cellCount = 6;
+var selectedIndex = 0;
 
-$(document).ready(function(){
-    $(".expertise-3").hover(function(){
-        $(".expertise-img-3").css("display", "inline");
-        }, function(){
-        $(".expertise-img-3").css("display", "none");
-    });
-});
+function rotateCarousel() {
+  var angle = selectedIndex / cellCount * -360;
+  carousel.style.transform = 'translateZ(-43.3vw) rotateY(' + angle + 'deg)';
+}
 
-$(document).ready(function(){
-    $(".expertise-4").hover(function(){
-        $(".expertise-img-4").css("display", "inline");
-        }, function(){
-        $(".expertise-img-4").css("display", "none");
-    });
-});
+function next() {
+	selectedIndex++;
+  	rotateCarousel();
 
-$(document).ready(function(){
-    $(".expertise-5").hover(function(){
-        $(".expertise-img-5").css("display", "inline");
-        }, function(){
-        $(".expertise-img-5").css("display", "none");
-    });
-});
+}
 
+function prev() {
+	selectedIndex--;
+  	rotateCarousel();
+
+}
+
+// var prevButton = document.querySelector('.previous-button');
+// prevButton.addEventListener('click', function() {
+//   selectedIndex--;
+//   rotateCarousel();
+// });
+
+// var nextButton = document.querySelector('.next-button');
+// nextButton.addEventListener('click', function() {
+//   selectedIndex++;
+//   rotateCarousel();
+// });
 
 
